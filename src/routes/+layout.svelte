1b01d8d5fba1dedc8f4b2d569146a1f3ee04b5e0
Jonas Hartmann <80170229+hartmann-jonas@users.noreply.github.com>
2023-11-20 17:42:24 +0100

    feat: update page title/description

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    
+    inject({ mode: dev ? 'development' : 'production' });
+</script>
+
+<svelte:head>
+    <title>FEG Downloads</title>
+	<meta name="og:title" content="Einverständniserklärungen des FEGs"/>
+	<meta name="description" content="Auf dieser Seite findest du alle Einverständniserklärungen zum Datenschutz des Friedrich-Ebert-Gymnasiums">
+</svelte:head>
